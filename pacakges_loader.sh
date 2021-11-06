3932b2ab8b233ae0fd45784b87080f313c36e513
余友竹 <31908038+chu0802@users.noreply.github.com>
2021-11-07 03:30:21 +0800

    Download all packages I'm using currently.

---
+++ pacakges_loader.sh
@@ -0,0 +1,12 @@
+mkdir -p bundle
+cd bundle
+git clone https://github.com/ncm2/ncm2
+git clone https://github.com/ncm2/ncm2-bufword
+git clone https://github.com/ncm2/ncm2-jedi
+git clone https://github.com/ncm2/ncm2-path
+git clone https://github.com/roxma/nvim-yarp
+git clone https://github.com/rakr/vim-one
+git clone https://github.com/vim-python/python-syntax
+git clone https://github.com/keith/swift.vim
+git clone https://github.com/preservim/nerdtree
+cd ..
